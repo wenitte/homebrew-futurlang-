107227ef6a7fcae636907b23fdc3bea56928cea1
Wenitte Apiou <wenitteapiou@gmail.com>
2026-04-20 22:34:42 -0400

    Fix formula: remove Language::Node (removed in Homebrew 4.x)

--- Formula/futurlang.rb
+++ Formula/futurlang.rb
@@ -8,8 +8,7 @@ class Futurlang < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    system "npm", "install", "-g", "--prefix", prefix, "futurlang@#{version}"
   end
 
   def caveats
